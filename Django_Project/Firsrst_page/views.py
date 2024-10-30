2faa9dd63a14fa4fa6f1f86a7d541cb95e3b4534
Sorumeit <kanievzhanserik7@gmail.com>
2024-10-30 22:22:56 +0500

    15

--- Django_Project/Firsrst_page/views.py
+++ Django_Project/Firsrst_page/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render( request, "Firsrst_page/index.html" )
+    return render( request, "Firsrst_page/index.html", {
+        "newyear": True
+    } )
