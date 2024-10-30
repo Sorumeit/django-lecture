34fbe23d433f79fee16057d8c13c2935c895d671
Sorumeit <kanievzhanserik7@gmail.com>
2024-10-30 23:24:29 +0500

    15

--- Django_Project/Django_Project/urls.py
+++ Django_Project/Django_Project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("Firsrst_page.urls"))
+    path('', include("Firsrst_page.urls"))
 ]
 
