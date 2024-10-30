4383fc6cfa0492ab06b01b4d86b7315b1210eed5
Sorumeit <kanievzhanserik7@gmail.com>
2024-10-30 23:23:50 +0500

    15

--- Django_Project/Firsrst_page/views.py
+++ Django_Project/Firsrst_page/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def index(request):
+    time = datetime.now();
     return render( request, "Firsrst_page/index.html", {
-        "newyear": datetime.now.day == 1 and datetime.now.month == 1
+        "newyear": time.day == 1 and time.month == 1
     } )
